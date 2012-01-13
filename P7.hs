77322f6bdc0e03e01639568d71d513d1d0603297
Nils Schweinsberg <mail@n-sch.de>
2012-01-13 14:55:25 +0100

    P7 solution

---
+++ P7.hs
@@ -0,0 +1,30 @@
+start :: Int -> Int
+start x = ceiling . sqrt . fi $ x
+ where
+  fi :: Int -> Double
+  fi = fromIntegral
+
+divsOf :: Int -> [Int]
+divsOf x = filter (\y -> x `mod` y == 0)
+                  [s, s-1 .. 2]
+ where
+  s = start x
+
+isPrim :: Int -> Bool
+isPrim x = null $ divsOf x
+
+
+--------------------------------------------------------------------------------
+--
+-- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+-- that the 6th prime is 13.
+--
+-- What is the 10 001st prime number?
+--
+--------------------------------------------------------------------------------
+
+sixth_prime :: Int
+sixth_prime = last $ take 6 $ filter isPrim [1..]
+
+p7 :: Int
+p7 = last $ take 10001 $ filter isPrim [1..]
