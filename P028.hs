23be7d9ae106c7199bbb0b609bcf0412fc48a0a7
Nils Schweinsberg <mail@nils.cc>
2015-01-31 02:18:29 +0100

    P028

--- P028.hs
+++ P028.hs
@@ -1,2 +1,19 @@
+import Test.HUnit
+
+spiralSum :: Integer -> Integer
+spiralSum l = go 1 2
+ where
+  go s step
+    | step < l  = go (s + 4 * (step-1)^2 + 10 * step)
+                     (step+2)
+    | otherwise = s
+
 main :: IO ()
-main = return ()
+main = do
+  print =<< runTests
+  print $ spiralSum 1001
+
+runTests :: IO Counts
+runTests = runTestTT $ TestList
+  [ 101 ~=? spiralSum 5
+  ]
